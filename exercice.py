8220d39699180620e5f9fe55baba8377ea636157
totyrola <astir.tadrous@polymtl.ca>
2020-09-15 15:43:01 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    (angle_rads * 180)
+    (angle_rads * 180) / math.pi
     return  
 
 
