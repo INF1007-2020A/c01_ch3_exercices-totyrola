373101208420fdc0787fc372b2e4a2dfcfcd641a
totyrola <astir.tadrous@polymtl.ca>
2020-09-15 15:57:19 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    angle_min
+    angle_mins * 2
     return 0.0
 
 
