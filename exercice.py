663b302d697e9af16c443e66f8a9ac7c7dc49c59
totyrola <astir.tadrous@polymtl.ca>
2020-09-15 16:05:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
     angle_mins /= 60
     angle_secs /= 3600
     angle_mins %= 1
-    angle_total = (angle_degs + angle_mins ) 
+    angle_total = (angle_degs + angle_mins + angle_secs ) 
 
 
     return 0.0
