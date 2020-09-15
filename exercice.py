ce9ce8021d88f68516c7f4e2b7e3dc1b213b4be5
totyrola <astir.tadrous@polymtl.ca>
2020-09-15 13:00:20 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return (temper)
+    return (temperatur)
 
 def to_farenheit(temperature: float) -> float:
     return 0.0
