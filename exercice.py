74eebb32dea25bd078e22862a6aebc78828c3e2e
totyrola <astir.tadrous@polymtl.ca>
2020-09-15 16:08:45 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def average(a: float, b: float, c: float) -> float:
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     angle_mins /= 60
-    angle_mins 
+    angle_mins //= 1
     angle_secs /= 3600
     angle_total = (angle_degs + angle_mins + angle_secs)
     angle_rad = (angle_total * math.pi) / 180
